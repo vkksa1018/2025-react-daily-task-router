abb3c0ab8a4cd5ed617a76402ee09d5f3ec4dc19
James <ohbwgab18@gmail.com>
2026-02-21 11:26:12 +0800

    day45

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
 import "./App.css";
-import { HashRouter, NavLink, Routes, Route } from "react-router-dom";
+import {
+  HashRouter,
+  NavLink,
+  Routes,
+  Route,
+  useParams,
+  Outlet,
+} from "react-router-dom";
 import Logout from "./components/Logout";
 
-const Todo = () => {
+const Todo = () => (
+  <div>
+    <p>這是 Todo 頁面</p>
+    <Logout />
+  </div>
+);
+const Login = () => <p>這是登入頁面</p>;
+const Register = () => <p>這是註冊頁面</p>;
+
+// 定義 Post (父元件) 與 PostId (子元件)
+
+const Post = () => {
   return (
     <div>
-      <p>這是 Todo 頁面</p>
-      <Logout />
+      <h2>Post 頁面</h2>
+      <div>
+        <Outlet />
+      </div>
     </div>
   );
 };
-const Login = () => {
-  return <p>這是登入頁面</p>;
-};
-const Register = () => {
-  return <p>這是註冊頁面</p>;
+
+const PostId = () => {
+  const { postId } = useParams();
+  return <p>Post ID: {postId}</p>;
 };
 
 function App() {
@@ -23,27 +42,33 @@ function App() {
       <HashRouter>
         <div className="nav-link">
           <NavLink to="/">
-            <p>回到首頁</p>
+            <p>Back</p>
           </NavLink>
           <NavLink to="/register">
-            <p>註冊頁面</p>
+            <p>Register Page</p>
           </NavLink>
           <NavLink to="/login">
-            <p>登入頁面</p>
+            <p>Login Page</p>
           </NavLink>
           <NavLink to="/todo">
-            <p>Todo 頁面</p>
+            <p>Todo Page</p>
+          </NavLink>
+
+          <NavLink to="/post/post123">
+            <p>Post 詳細頁面</p>
           </NavLink>
         </div>
-        {/* Routes, Route 練習區 */}
+
         <Routes>
+          <Route path="/" element={<p>這是首頁內容</p>} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/todo" element={<Todo />} />
-          <Route path="/" element={<p>這是首頁內容</p>} />
-        </Routes>
 
-        {/* 練習區 */}
+          <Route path="/post" element={<Post />}>
+            <Route path=":postId" element={<PostId />} />
+          </Route>
+        </Routes>
       </HashRouter>
     </div>
   );
